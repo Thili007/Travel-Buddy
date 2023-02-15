925376b7b761b8128ae6ba8e24af47b31f4b85f9
S Rabi <rabi.sanaye@yahoo.com>
2023-02-15 15:06:18 +0100

    client added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import PostCard from "./components/PostCard";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <PostCard />
+    </div>
+  );
+}
+
+export default App;
