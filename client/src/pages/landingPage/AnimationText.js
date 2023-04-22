2ee52b459bbc0416b540fdc43d1580bdf8c3ac9f
Thilina Punchi Hewage <94338685+Thili007@users.noreply.github.com>
2023-04-23 01:16:28 +0200

    updated animation for landing page

---
+++ client/src/pages/landingPage/AnimationText.js
@@ -0,0 +1,23 @@
+import "./landingPage.css";
+
+export const AnimationText = () => {
+  return (
+    <div className="wrapper">
+      <div className="static-txt">Hello </div>
+      <ul className="dynamic-txts">
+        <li>
+          <span>Welcome To Travel Buddy</span>
+        </li>
+        <li>
+          <span>Plan Trip</span>
+        </li>
+        <li>
+          <span>Make New Friends</span>
+        </li>
+        <li>
+          <span>Discover The World</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
